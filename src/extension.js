9817a198564700f79653f6a771f78741bfe89af6
kmkingdon <32685092+kmkingdon@users.noreply.github.com>
2018-04-13 15:55:03 -0600

    access subject of newly composed email

--- src/extension.js
+++ src/extension.js
@@ -6,7 +6,20 @@ const GmailFactory = require("gmail-js");
 const gmail = new GmailFactory.Gmail($);
 window.gmail = gmail;
 
+//Built in observe method from the boilerplate- used to check if extension is running 
 gmail.observe.on("load", () => {
     const userEmail = gmail.get.user_email();
     console.log("Hello, " + userEmail + ". This is your extension talking!");
 });
+
+//Identify the subject of the new composed email
+gmail.observe.on("compose", () => {
+    const subject = document.getElementsByName('subjectbox')[0].onchange=findSubject;
+})
+
+function findSubject(event) {
+    console.log('changing')
+    console.log(event.target.value)
+}
+
+
